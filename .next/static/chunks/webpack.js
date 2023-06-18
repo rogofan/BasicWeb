2d30da5002705171e2d53abd77b4a638e5403d83
rogofan <sacokov@gmail.com>
2023-06-18 15:02:59 +0200

    LastCommit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9d54d0e2ac9d5f93"; }
+/******/ 		__webpack_require__.h = function() { return "4b6305c7c0c81837"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
